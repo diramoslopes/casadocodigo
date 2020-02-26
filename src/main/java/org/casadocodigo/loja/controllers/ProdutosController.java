98626ae00be182983ed553c56cc0e0718945a4ea
Diogo <dramoslopesilveira@gmail.com>
2020-02-26 19:00:21 -0300

    redirect flash attributes

--- src/main/java/org/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/org/casadocodigo/loja/controllers/ProdutosController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/produtos")
@@ -27,10 +28,13 @@ public class ProdutosController {
 	}
 	
 	@PostMapping
-	public String gravar(Produto produto) {
+	public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes) {
 		System.out.println(produto);
 		produtoDAO.gravar(produto);
-		return "produtos/ok";
+		
+		redirectAttributes.addFlashAttribute("sucesso", "produto cadastrado com SUCESSO!");
+		
+		return  new ModelAndView("redirect:produtos");
 	}
 	
 	@GetMapping
